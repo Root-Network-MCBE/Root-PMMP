18d0db3efbabdb50b0135e433187d9cbc120c0f2
Valres <104681026+ValresMC@users.noreply.github.com>
2025-10-03 17:14:05 +0200

    .

--- build/server-phar.php
+++ build/server-phar.php
@@ -142,7 +142,7 @@ function main() : void{
 	if(isset($opts["git"])){
 		$gitHash = $opts["git"];
 	}else{
-		$gitHash = "84961050ff272f840c92564db68d8769b8ca7fd5";
+		$gitHash = "4937a82efe4d4320a2eb714821b30d0ff9f911ad";
 		echo "Git hash detected as $gitHash" . PHP_EOL;
 	}
 	if(isset($opts["build"])){
